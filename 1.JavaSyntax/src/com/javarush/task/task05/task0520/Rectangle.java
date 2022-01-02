1c040ad00ce196d6cb6bf4aaed29ce735241a8da
p_Ilya <petermoloch@mail.ru>
2022-01-02 12:02:07 +0300

    02/01/22

--- 1.JavaSyntax/src/com/javarush/task/task05/task0520/Rectangle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0520/Rectangle.java
@@ -7,6 +7,38 @@ package com.javarush.task.task05.task0520;
 
 public class Rectangle {
     //напишите тут ваш код
+    private int top;
+    private int left;
+    private int width;
+    private int height;
+
+    public Rectangle(int top, int left, int width, int height) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int top, int left) {
+        this.top = top;
+        this.left = left;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public Rectangle(int top, int left, int width) {
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = width;
+    }
+
+    public Rectangle(Rectangle rect){
+        this.top = rect.top;
+        this.height = rect.height;
+        this.width = rect.width;
+        this.left = rect.left;
+    }
 
     public static void main(String[] args) {
 
