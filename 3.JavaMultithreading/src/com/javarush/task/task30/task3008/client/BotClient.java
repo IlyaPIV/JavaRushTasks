27d0c2241cab260c2e5927780a201bc582d2bcc0
p_Ilya <petermoloch@mail.ru>
2022-06-26 12:51:59 +0300

    26/06/2022

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,16 +1,67 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Random;
 
 public class BotClient extends Client{
 
     public class BotSocketThread extends SocketThread {
 
-    }
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            BotClient.this.sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
-    @Override
-    protected SocketThread getSocketThread() {
-        return new BotSocketThread();
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+
+            // Отделяем отправителя от текста сообщения
+            String userNameDelimiter = ": ";
+            String[] split = message.split(userNameDelimiter);
+            if (split.length != 2) return;
+
+            String messageWithoutUserName = split[1];
+
+            // Подготавливаем формат для отправки даты согласно запросу
+            String format = null;
+            switch (messageWithoutUserName) {
+                case "дата":
+                    format = "d.MM.YYYY";
+                    break;
+                case "день":
+                    format = "d";
+                    break;
+                case "месяц":
+                    format = "MMMM";
+                    break;
+                case "год":
+                    format = "YYYY";
+                    break;
+                case "время":
+                    format = "H:mm:ss";
+                    break;
+                case "час":
+                    format = "H";
+                    break;
+                case "минуты":
+                    format = "m";
+                    break;
+                case "секунды":
+                    format = "s";
+                    break;
+            }
+
+            if (format != null) {
+                String answer = new SimpleDateFormat(format).format(Calendar.getInstance().getTime());
+                BotClient.this.sendTextMessage("Информация для " + split[0] + ": " + answer);
+            }
+        }
     }
 
     @Override
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/ClientGuiController.java
@@ -0,0 +1,70 @@
+package com.javarush.task.task30.task3008.client;
+
+public class ClientGuiController extends Client{
+
+    private final ClientGuiModel model = new ClientGuiModel();
+    private final ClientGuiView view = new ClientGuiView(this);
+
+    public class GuiSocketThread extends SocketThread {
+        @Override
+        protected void processIncomingMessage(String message) {
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        protected void informAboutAddingNewUser(String userName) {
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void informAboutDeletingNewUser(String userName) {
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    @Override
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        // Нет необходимости запускать SocketThread в отдельном потоке
+        socketThread.run();
+    }
+
+
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+    public ClientGuiModel getModel() {
+        return model;
+    }
+
+    public static void main(String[] args) {
+        ClientGuiController client = new ClientGuiController();
+        client.run();
+    }
+}
