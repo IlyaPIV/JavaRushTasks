1cc4351840eb32487b5b1c589ad351a866f17ca5
p_Ilya <petermoloch@mail.ru>
2022-07-10 21:57:15 +0300

    10/07/2022

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,126 @@
+package com.javarush.task.task32.task3209;
+
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
+
+import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+
+    private UndoManager undoManager = new UndoManager();;
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException e) {
+            ExceptionHandler.log(e);
+        }
+
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public void init(){
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
+    }
+
+    public void initMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, menuBar);
+        MenuHelper.initEditMenu(this, menuBar);
+        MenuHelper.initStyleMenu(this, menuBar);
+        MenuHelper.initAlignMenu(this, menuBar);
+        MenuHelper.initColorMenu(this, menuBar);
+        MenuHelper.initFontMenu(this, menuBar);
+        MenuHelper.initHelpMenu(this, menuBar);
+        getContentPane().add(menuBar, BorderLayout.NORTH);
+    }
+
+    public void initEditor(){
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.add("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.add("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(600, 400));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void selectedTabChanged(){
+
+    }
+
+    public boolean canUndo(){
+        return undoManager.canUndo();
+    }
+
+    public void undo(){
+        try {
+            undoManager.undo();
+        } catch (CannotUndoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public boolean canRedo(){
+        return undoManager.canRedo();
+    }
+
+    public void redo(){
+        try {
+            undoManager.redo();
+        } catch (CannotRedoException e) {
+            ExceptionHandler.log(e);
+        }
+
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
+    public void resetUndo(){
+        undoManager.discardAllEdits();
+    }
+
+
+    public boolean isHtmlTabSelected(){
+        return tabbedPane.getSelectedIndex() == 0;
+    }
+    public void exit() {
+        controller.exit();
+    }
+}
